8cb4ba846329f8d4389bb8a35f1bd6b88ccf325a
dotnotation <dotinmotionproductions@gmail.com>
2022-02-07 16:51:18 -0500

    commit tree 57b7f6cabc828779af4aac8fd8a539a623e6d3e5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
-// create your App component here
+import React, { Component } from "react"
+
+class App extends Component {
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(json => this.setState({data: json}))
+    }
+
+    render(){
+        return (
+            <h1>"Hello"</h1>
+        )
+    }
+}
+
+export default App 
